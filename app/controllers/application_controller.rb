afbed117dcf23f6dccf2540efa29c3bece16aa72
kazami-natsuki <67785186+kazami-natsuki@users.noreply.github.com>
2020-09-17 17:31:40 +0900

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,8 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   add_flash_types :success, :info, :warning, :danger
-
+  
+  # 課題
   helper_method :current_user, :logged_in?
 
   def current_user
